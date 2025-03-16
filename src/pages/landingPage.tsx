b1cbcac74507a16511535208665c87ae88443f25
Sharad Banga <sharadiit03@gmail.com>
2025-03-16 21:50:03 +0530

    update

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -49,16 +49,19 @@ export  function LandingPage() {
 
           <div className="z-1 flex justify-center mt-8 sm:hidden">
 
-                <div className=" mt-0 h-[220px] w-[90vw] bg-[url('./images/rainbow-gradii.png')] bg-cover bg-center bg-no-repeat rounded-lg rotate-180 ">
+                <div className=" mt-0 h-[250px] w-[90vw] bg-[url('./images/rainbow-gradii.png')] bg-cover bg-center bg-no-repeat rounded-lg rotate-180 ">
 
                 </div>
 
-                <div className="text-white absolute font-extrabold flex-col justify-between  sm:h-[40vh] text-[200%] md:text-[60px] mt-5   w-[100vw] font-dela-gothic text text-nowrap z-10   ">
+                <div className="text-white absolute font-normal flex-col justify-between  sm:h-[40vh] text-[200%] md:text-[60px] mt-5   w-[100vw] font-dela-gothic text text-nowrap z-10   ">
 
                     <div className="flex justify-center"> ONE &nbsp; STOP </div>
 
                     <div className="flex justify-center  w-full text-[90%]"> FOR &nbsp; DEVELOPERS</div>
-                
+
+                  <div className="mt-4 text-[13px] font-cmono  flex justify-center">
+                    Be Productive with devHub tools.
+                  </div>
                   
                 
                     <div className="  flex justify-center mt-8">
