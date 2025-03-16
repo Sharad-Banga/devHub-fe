f82419af71239fe53d46ef0e21ed95ae3f2939a7
Sharad Banga <sharadiit03@gmail.com>
2025-03-16 20:45:53 +0530

    update

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "../landingPageComponents/navbar";
 
 export  function LandingPage() {
     return (
-        <div className="bg-black w-screen h-screen bg-[url('./images/blue-gradii.png')] bg-cover bg-left-top"  >
+        <div className="bg-black w-screen h-screen bg-[url('./images/mob-bg.png')]  sm:bg-[url('./images/blue-gradii.png')] bg-cover bg-left-top"  >
 
           <Navbar />
           <br />
