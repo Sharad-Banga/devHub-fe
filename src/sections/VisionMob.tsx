66492bdce0546a65db7b7363de79a76761cd5bb4
Sharad Banga <sharadiit03@gmail.com>
2025-03-18 03:20:48 +0530

    update

--- src/sections/VisionMob.tsx
+++ src/sections/VisionMob.tsx
@@ -4,15 +4,8 @@ export const VisionMob =()=>{
     <>
         <div className="sm:hidden  h-[400px] mt-20 flex justify-center items-center flex-col ">
 
-
-            
-
-
-          
-
           <div className="bg-[url('./images/vision_img.png')]   bg-center   w-[80vw] h-[300px] rounded-md flex-row">
           
-
 
                     <div className="flex-col  mt-7  text-white text-[8.5vw]  font-dela-gothic">
 
@@ -37,7 +30,7 @@ export const VisionMob =()=>{
                       </div> */}
 
 
-                      <div className="flex-col  w-screen  ">
+                      <div className="flex-col tracking-wider  w-screen  ">
 
                           <div className="flex justify-center ">
                           Seamless environment where
