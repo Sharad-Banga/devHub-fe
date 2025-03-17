3114e8b580c4aaffb87f984cd6fc0d965620a7fb
Sharad Banga <sharadiit03@gmail.com>
2025-03-17 11:52:07 +0530

    update

--- src/sections/VisionMob.tsx
+++ src/sections/VisionMob.tsx
@@ -5,16 +5,20 @@ export const VisionMob =()=>{
         <div className=" h-[300px] w-full flex justify-center items-center flex-col ">
 
 
-        <div className="absolute mb-24 font-normal text-[120%] flex justify-center font-dela-gothic  text-white ">
-            Empowering Developers
-            <br />
-            with the Tools They Need
-          </div>
+            <div className="absolute mb-24 font-normal text-[170%]  font-dela-gothic w-screen   text-white ">
+
+              <div className="flex justify-center">
+              Empowering
+              Developers
+              </div>
+              
+              
+            </div>
 
 
           
 
-          <div className="bg-[url('./images/vision_img.png')] bg-center mt-5 image-rendering:-webkit-optimize-contrast w-[80vw] h-[130px] rounded-md "></div>
+          <div className="bg-[url('./images/vision_img.png')] bg-center mt-4 image-rendering:-webkit-optimize-contrast w-[80vw] h-[130px] rounded-md "></div>
 
         </div>
     </>
